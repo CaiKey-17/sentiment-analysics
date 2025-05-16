5b18f67dadd8287915596059fc4fd7e5ffbdb4de
CaiKey-17 <163799447+CaiKey-17@users.noreply.github.com>
2025-05-16 11:16:36 +0700

    Update app.py

--- app.py
+++ app.py
@@ -217,7 +217,10 @@ models = {
     "result": best_svc_model,
 }
 from flask import Flask, request, jsonify
+from flask_cors import CORS  
+
 app = Flask(__name__)
+CORS(app)  
 
 @app.route('/predict', methods=['POST'])
 def predict():
